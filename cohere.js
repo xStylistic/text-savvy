b736f4500741b70988604188f06921cfeae58bc7
kimjere5 <jeremydaniel.kim@mail.utoronto.ca>
2025-03-21 22:17:25 -0400

    saving

--- cohere.js
+++ cohere.js
@@ -1,22 +1,6 @@
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
     if (request.action === "callCohere") {
 
-      // const { CohereClientV2 } = require('cohere-ai');
-      // const cohere = new CohereClientV2({
-      //   token: 'WUAo4EcWA7WJxL9fX9S8J40X8tq2iyyXameQEQIz',
-      // });
-      // (async () => {
-      //   const response = await cohere.chat({
-      //     model: 'command-a-03-2025',
-      //     messages: [
-      //       {
-      //         role: 'user',
-      //         content: 'who is the president of the philippines',
-      //       },
-      //     ],
-      //   });
-      //   console.log(response.message.content);
-      // })();
       const res = await fetch("https://api.cohere.ai/v1/generate", {
         method: "POST",
         headers: {
